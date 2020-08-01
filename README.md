88e1aa38c8799e15462f9fa6a5762a70fcb7f3be
egimoro <gimoremma@gmail.com>
2020-08-02 00:09:44 +0200

    Pretty printed

---
+++ README.md
@@ -0,0 +1,5 @@
+#Flask Ajax example1
+
+Post requet in forms using Pretty Printed
+
+https://www.youtube.com/watch?v=IZWtHsM3Y5A
---
+++ static/js/form.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+                    $("form").on("submit", function () {
+                    $.ajax({
+                      data: {
+                        name: $("#nameInput").val(),
+                        email: $("#emailInput").val()
+                      },
+                      type : "POST",
+                      url: "/process"
+                    })
+                    .done(function (data) {
+                      if (data.error) {
+                        $("#errorAlert").text(data.error).show()
+                        $("#successAlert").hide();
+                      }
+                      else {
+                        $("#successAlert").text(data.name).show()
+                        $("errorAlert").hide();
+                        
+                      }
+                            
+                          })
+                     event.preventDefault();                        
+                                           })
+                  })
